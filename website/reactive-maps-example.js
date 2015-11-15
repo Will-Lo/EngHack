dc00f07d7a222c967b7fa9ded2689db8ceccced7
emilyki <emilyki@outlook.com>
2015-11-14 22:36:21 -0500

    Modifications Made
    The following modifications have been made to the code:
    Google maps location first initialized to E5, UWaterloo
    E5 has been marked with a (static) marker

--- website/reactive-maps-example.js
+++ website/reactive-maps-example.js
@@ -1,8 +1,20 @@
 Markers = new Mongo.Collection('markers');
 
 if (Meteor.isClient) {
+
   Template.map.onCreated(function() {
     GoogleMaps.ready('map', function(map) {
+
+      var latLng = {lat: 43.472848, lng: -80.540266};
+
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(latLng.lat, latLng.lng),
+        map: map.instance,
+        id: document._id
+      });
+       // Markers.insert({ lat: 43.472848, lng: -80.540266 });
+        //Markers.update(marker.id, { set: { lat: 43.472848, lng: -80.540266 } });
+
       google.maps.event.addListener(map.instance, 'click', function(event) {
         Markers.insert({ lat: event.latLng.lat(), lng: event.latLng.lng() });
       });
@@ -20,7 +32,7 @@ if (Meteor.isClient) {
           });
 
           google.maps.event.addListener(marker, 'dragend', function(event) {
-            Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
+            Markers.update(marker.id, { set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
           });
 
           markers[document._id] = marker;
@@ -34,6 +46,8 @@ if (Meteor.isClient) {
           delete markers[oldDocument._id];
         }
       });
+
+
     });
   });
 
@@ -46,9 +60,10 @@ if (Meteor.isClient) {
       if (GoogleMaps.loaded()) {
         return {
           center: new google.maps.LatLng(43.472848, -80.540266),
-          zoom: 8
+          zoom: 16
         };
       }
     }
   });
+
 }
